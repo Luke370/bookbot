8e25e250e11b03c59e6f0fd24156f1c3a0b364a7
Luke <2009lukeevans@gmail.com>
2025-07-13 15:37:35 -0400

    prints out information in a sorted and readable format

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from stats import get_num_words
 from stats import get_chars
+from stats import sort_data
 
 def main():
+    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...")
+
     num_words = get_num_words("./books/frankenstein.txt")
-    print(f"{num_words} words found in the document")
-    chars = get_chars("./books/frankenstein.txt")
-    print(chars)
+    print(f"----------- Word Count ----------\nFound {num_words} total words")
+    print("--------- Character Count -------")
+    chars = sort_data("./books/frankenstein.txt")
+    for c in chars:
+        print(f"{c["char"]}: {c["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,12 +1,14 @@
 def get_book_text(path):
     with open(path) as p:
         text = p.read()
+
     return text
 
 def get_num_words(path):
     text = get_book_text(path)
     words = text.split()
     num_words = len(words)
+
     return num_words
 
 def get_chars(path):
@@ -19,5 +21,19 @@ def get_chars(path):
             chars[t] += 1
     
     return chars
+
+def sort_data(path):
+    chars = get_chars(path)
+    letters = []
+
+    for c in chars:
+        if c.isalpha():
+            letters.append({"char": c, "num": chars[c]})
+
+    sorted_chars = sorted(letters, key=lambda item: item["num"], reverse=True)
+
+    return sorted_chars
+
+
 
 
